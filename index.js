722494d6873fe797285414ca14ca9647a2457cce
Jinming Wu, Patrick <me@patrickwu.space>
2022-04-08 22:47:02 +0800

    update date generation

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ hexo.config.opengraph_image = Object.assign({
   font_color: "#000",
   title_font: "Bold 70pt Arial",
   date_font: "Regular 30pt Arial",
+  date_style: "YYYY/MM/DD",
 }, hexo.config.opengraph_image);
 
 const config = hexo.config.opengraph_image;
@@ -21,3 +22,8 @@ hexo.extend.generator.register('opengraph_image', locals => {
     return ogimage.call(hexo, locals);
 });
 
+hexo.extend.helper.register('opengraph_image', function(){
+    const url_for = hexo.extend.helper.get('url_for').bind(hexo);
+    const is_post = hexo.extend.helper.get('is_post').bind(hexo);
+    return '<meta property="og:image" content="https://cdn.patrickwu.space/base/social.png" />';
+});
